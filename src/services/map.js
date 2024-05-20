7e73abde2f114c855fc371ea20da6ead6f180d0b
ian eddie <ianeddie2@gmail.com>
2024-05-19 23:40:53 -0300

    optimizing seo part 2

--- src/services/map.js
+++ src/services/map.js
@@ -1,14 +1,1 @@
-// -34.57794937291372, -58.42668142744496
-
-const map = L.map('map').setView([-34.578459532847724, -58.42843895289317], 11)
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  maxZoom: 19
-}).addTo(map)
-
-const circle = L.circle([-34.578459532847724, -58.42843895289317], {
-  color: 'red',
-  fillColor: '#f03',
-  fillOpacity: 0.2,
-  radius: 4000
-}).addTo(map)
+const map=L.map("map").setView([-34.578459532847724,-58.42843895289317],11);L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png",{maxZoom:19}).addTo(map);const circle=L.circle([-34.578459532847724,-58.42843895289317],{color:"red",fillColor:"#f03",fillOpacity:.2,radius:4e3}).addTo(map);
